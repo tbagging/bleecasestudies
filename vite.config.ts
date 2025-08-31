b81a215f748f8016c4a70956f0d3e20039377385
tbagging <Tomer@thedreamshop.ai>
2025-08-31 16:46:53 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',            // keep '/' if you’re using your custom domain
+  base: '/',   // keep '/' if you’re using your custom domain
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
