0b0b8c147882a2e161979e425c410113a4351ca8
tbagging <Tomer@thedreamshop.ai>
2025-08-31 17:32:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                         // using your custom domain
-  resolve: { alias: { '@': '/src' } } // fixes @/imports
+  base: '/', // using your custom domain
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
